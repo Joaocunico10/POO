679acd281cd0b170f86c9fb115aa074623652912
Joao Paulo <engs-joaocunico@camporeal.edu.br>
2026-02-19 23:44:43 -0300

    POO - Exercicios 1

---
+++ Trabalho de  java 1/exercicio1.java
@@ -0,0 +1,12 @@
+public class exercicio1 {
+    public static void main(String[] args) {
+        int peso = 78;
+        double altura = 1.74;
+        double resultado = (peso/(altura*altura));
+
+        System.out.println("O programa ira caldular o IMC de uma pessoa");
+        System.out.println(resultado);
+        
+    }
+
+}
---
+++ Trabalho de  java 1/exercicio3.java
@@ -0,0 +1,14 @@
+public class exercicio3 {
+    public static void main(String[] args) {
+        int numero = 11;
+
+        System.out.println("O programa ira ver se um numero e par ou impar");
+
+        if(numero % 2 == 0){
+            System.out.println("O numero e par");
+        }
+        else{
+            System.out.println("Numero impar");
+        }
+    }
+}
